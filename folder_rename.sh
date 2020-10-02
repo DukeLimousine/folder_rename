67f19f2ae47e20c91fd8aa20112d23a5ca9d2da2
DukeLimousine <ljd82582@gmail.com>
2020-10-02 17:26:22 -0400

    Add files via upload
    Added basic rename functionality & printing to console

--- folder_rename.sh
+++ folder_rename.sh
@@ -5,7 +5,7 @@ if [ "$1" != "" ]; then
 		echo "Are you sure $directory is the correct folder? Press y / Y + enter if yes. Otherwise, any other key and / or enter to exit."
 		read input </dev/tty
 				if [[ "$input" == "y" || "$input" == "Y" ]]; then 
-					continue
+					echo "Continuing..."
 				else
 					exit
 				fi
@@ -18,14 +18,15 @@ else
 	exit
 fi
 
-for filepath in "$directory"/*; do 
+for filepath in "$directory"/*; do
+	echo "Inspecting $filepath"
 	for file in "$filepath"/*; do
-		echo "$(basename -- $filepath)"
-		echo "$(basename -- $file)"
 		if [[ -d $file ]]; then
 			echo "$file is a directory"
 		elif [[ -f $file ]]; then
-			echo "$file is a file"
+			basefilepath="$(basename -- $filepath)"
+			echo "Renaming $file to $filepath/${basefilepath}.${file##*.}"
+			mv "$file" "$filepath/${basefilepath}.${file##*.}"
 	 	else
 	 		echo "$file is not valid"
 	 		exit 1
