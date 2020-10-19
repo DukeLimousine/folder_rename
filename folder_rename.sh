9a6054218fe95f3691f080fee1cb9fba22801f08
DukeLimousine <ljd82582@gmail.com>
2020-10-19 10:59:00 -0400

    Add files via upload

--- folder_rename.sh
+++ folder_rename.sh
@@ -21,12 +21,24 @@ fi
 for filepath in "$directory"/*; do
 	echo "Inspecting $filepath"
 	for file in "$filepath"/*; do
+		#Ensure filetype is file
 		if [[ -d $file ]]; then
 			echo "$file is a directory"
 		elif [[ -f $file ]]; then
 			basefilepath="$(basename -- $filepath)"
-			echo "Renaming $file to $filepath/${basefilepath}.${file##*.}"
-			mv "$file" "$filepath/${basefilepath}.${file##*.}"
+			finalname="$filepath/${basefilepath}.${file##*.}"
+			#Test for potential overwrites
+			if test -f "$finalname"; then
+				echo "Skip renaming $file ? This will overwrite another file that has already been renamed. Press N + enter to overwrite (not reccommended) or any other key + enter to skip."
+				read input </dev/tty
+				if [[ "$input" == "n" || "$input" == "N" ]]; then 
+					echo "Continuing..."
+				else
+					continue
+				fi
+			fi
+			echo "Renaming $file to $finalname"
+			mv "$file" "$finalname"
 	 	else
 	 		echo "$file is not valid"
 	 		exit 1
